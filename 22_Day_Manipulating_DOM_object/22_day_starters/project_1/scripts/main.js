be5a5fbd5e155c1adef96f9163014b4bb15d8ba8
Henry Guido <79233975+hguidob@users.noreply.github.com>
2023-03-31 23:00:45 -0600

    Day 22 project_1

--- 22_Day_Manipulating_DOM_object/22_day_starters/project_1/scripts/main.js
+++ 22_Day_Manipulating_DOM_object/22_day_starters/project_1/scripts/main.js
@@ -1,2 +1,29 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+
+
+
+const container = document.querySelector('.wrapper')
+
+
+
+function primeNumber(num){
+    const primeNumbers = []
+    for(let i = 2; i <= num; i++){
+        if(i % i === 0 && i % 1 === 0 && primeNumbers.every((item) => i % item !== 0
+        )){
+            primeNumbers.push(i)
+        }
+    }
+    return primeNumbers.includes(num)
+}
+
+for(let i = 0; i <= 101; i++){
+    if(primeNumber(i)){
+        container.innerHTML += `<div class="box prime">${i}</div>`
+    }
+    else if (i % 2 === 0){
+        container.innerHTML += `<div class="box even">${i}</div>`
+    }
+    else{
+        container.innerHTML += `<div class="box odd">${i}</div>`
+    }
+}
