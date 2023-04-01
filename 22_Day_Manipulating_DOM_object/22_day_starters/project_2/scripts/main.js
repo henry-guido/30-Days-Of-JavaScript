95e107fbe78a1eec18ad9c83f161614789c4cfe2
Henry Guido <79233975+hguidob@users.noreply.github.com>
2023-03-31 23:05:49 -0600

    Day 22 project_2

--- 22_Day_Manipulating_DOM_object/22_day_starters/project_2/scripts/main.js
+++ 22_Day_Manipulating_DOM_object/22_day_starters/project_2/scripts/main.js
@@ -1,1 +1,12 @@
-console.log(countries)
+const countriesContainer = document.querySelector('#total-countries')
+const countriesWrapper = document.querySelector('.countries-wrapper')
+
+const totalCountry = countries.length
+
+
+for (let i = 0; i < totalCountry; i++) {
+    countriesWrapper.innerHTML += `<div>${countries[i]}</div>`
+}
+
+
+countriesContainer.textContent = `Total Number of countries: ${totalCountry}`
